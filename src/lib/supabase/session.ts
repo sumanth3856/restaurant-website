dc79f83fe03e2285de615bd5348a94f42a9830a6
sumanth <saisumanth3856@gmail.com>
2025-12-12 00:52:04 +0530

    fix: enable middleware with env var safety check

--- src/lib/supabase/session.ts
+++ src/lib/supabase/session.ts
@@ -7,9 +7,14 @@ export async function updateSession(request: NextRequest) {
         request,
     })
 
+    if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+        console.error('Supabase Entironment Variables are missing in Middleware!')
+        return supabaseResponse
+    }
+
     const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
         {
             cookies: {
                 getAll() {
